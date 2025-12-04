3caeb20ff92d92db6e865c9107da6feb6c85d5b8
nikhilpant05 <niksuniverse05@gmail.com>
2025-12-04 21:10:59 +0530

    DONE

---
+++ exp2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("Nikhil\n");
+    printf("262501 Sector 69 Pithoragarh\n");
+    printf("Uttarakhand\n");
+    printf("India\n");
+    return 0;
+}
